4f19c11993c03e2f4e49f07ae76bb15b4368ee6e
Fahrudin A'hmad <fahrudinasevenfold@gmail.com>
2024-09-19 21:42:17 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        $this->call(UsersSeeder::class);
         $this->call(RumahSeeder::class);
         $this->call(IuranSeeder::class);
         $this->call(PenghuniSeeder::class);
